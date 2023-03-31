6fbe1f3b0d6bfca7514df8cd352c375888d4b059
Gelato <you@example.com>
2023-03-31 13:34:21 +0300

    test

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -3,17 +3,20 @@ import { setContext } from "@apollo/client/link/context";
 
 export /* async */ function getClientOptions(/* {app, router, ...} */ options) {
 	const httpLink = createHttpLink({
-		uri: "https://firm-dassie-25.hasura.app/v1/graphql",
+		// uri: "https://firm-dassie-25.hasura.app/v1/graphql",
+		uri: "https://app.dev.druid.1t.ru/graphql",
 	})
 
 	const authLink = setContext((_, { headers }) => {
 		// get the authentication token from local storage if it exists
-		const token = sessionStorage.getItem("token");
+		// const token = sessionStorage.getItem("token");
 		// return the headers to the context so httpLink can read them
 		return {
 			headers: {
 				...headers,
-				Authorization: token ? `Bearer ${token}` : "",
+				// Authorization: token ? `Bearer ${token}` : "",
+				Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI3IiwianRpIjoiNmU5ZmEzODAzODI4YjE4ZjFjYTIzYmIxNDY2YzI1ZWUwZTE2ODVmZDllZmNjOGQwMzk1ZTQ1NDFmMzhhMzA4NmI4OGZhNDgzMGE0MDEyMGEiLCJpYXQiOjE2ODAyMDI1ODAuMjc1NDU5LCJuYmYiOjE2ODAyMDI1ODAuMjc1NDYxLCJleHAiOjE3MTE4MjQ5ODAuMjY3Mjk1LCJzdWIiOiI1OTcyMDg2NDY0MTY0Njk1MDkzIiwic2NvcGVzIjpbXX0.POMcll2ADVgqwCSBeOhvgUe5YPzisfHDAJaK5hYopkp5a7rV7kw0zsqNc0fH25Ol5AXiHxCe5XFgHQvAb7UGovTtfpLbrrCUfG90Y81-TCCAsd7yU7hw3FtEBgf6MfBUfFYSCgLFyaxOfyFKq9N5TmqPGI1uxaR21Q51gGCA5jnzI-t9WeTfmMu1oEh1l74tDZ9OKIWuuG-76nNgEOl-ky0JdL7m7gYgLBx8muCR7L3p2ZvQUbzZJloSeWLlsHkENPX2Tr5k2pc_KpDCwngEgBP4xR43mbR0Mw06YoYY2vLztA-1DtnzcVwBMu6MMaNj-u1Tu_ZEpvs8YOzHNE5ocRxso0_unB__kgEXzzw2eacnJK7XnCK4AXJf0bEYLlZykHh6pU_O7ydKs1dZrfQmPGLS8NFIntIrMdypCXJfNGLVZSlWzDiaEzGnWnSJUtaUUxkjXMGx7DTfIHcA7ju8ePdVFGTKOS8pfEgNhfR6VA7EidnWKvQL68HliivdgTZrEUwZVvHVa_BKT-XJS3LkrTKuI-JL87CybFwi4OdpiPxcjZcVKqPrHCsDEiI8qf9ut9uqAPOLCQCeZ3xcl7Td8hIJ3Hopum02NGFRy1iTXMzxYEXRJV_LBK8aE0aMQcHKLTbG24YLgJS8w_FuL7_kmP3-YnIEnMpPJbQQLRpaVHQ&space=727
+				`,
 			}
 		}
 	})
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,31 +11,15 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header> Essential Links </q-item-label>
       </q-list>
     </q-drawer>
 
@@ -46,71 +30,112 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import { defineComponent, ref } from "vue";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import { useMutation } from "@vue/apollo-composable";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
-  components: {
-    EssentialLink
-  },
+  setup() {
+    const leftDrawerOpen = ref(false);
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+    //Получение всех страниц
+    const { onResult } = useQuery(
+      gql`
+        query {
+          pages(perPage: 10, page: 1) {
+            data {
+              id
+              parent_id
+              page_type
+              title
+              content
+              icon
+              level
+              is_public
+              is_block
+              position
+              config
+              created_at
+              updated_at
+              object {
+                id
+                type_id
+              }
+            }
+            paginatorInfo {
+              perPage
+              count
+              total
+              currentPage
+              from
+              to
+              lastPage
+              hasMorePages
+            }
+          }
+        }
+      `
+    );
+    onResult((queryResult) => {
+      console.log(queryResult.data);
+    });
+
+    //Получение всех типов
+    {
+      const { onResult } = useQuery(
+        gql`
+          {
+            types(perPage: 10, page: 1) {
+              data {
+                id
+                name
+                label
+                description
+                system
+                searchable
+                id_ai
+                author_id
+                meta {
+                  headline
+                  icon
+                }
+                created_at
+                updated_at
+                properties {
+                  id
+                  name
+                  data_type
+                  label
+                }
+              }
+              paginatorInfo {
+                perPage
+                currentPage
+                lastPage
+                total
+                count
+                from
+                to
+                hasMorePages
+              }
+            }
+          }
+        `
+      );
+      onResult((queryResult) => {
+        console.log(queryResult.data);
+      });
+    }
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
 </script>
